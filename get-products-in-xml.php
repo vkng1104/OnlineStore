70373fa5ac75d572009c9546b710784e54357a69
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-23 20:29:03 +0700

    feat: create search input to find product

--- get-products-in-xml.php
+++ get-products-in-xml.php
@@ -1,17 +1,24 @@
 <?php
+// Connect to database
 include('./database/db_helper.php');
 $pdo = connectToDatabase();
 
+// Check if a search query is provided
+$searchQuery = isset($_GET['search']) ? $_GET['search'] : '';
+
 try {
-    $stmt = $pdo->query("SELECT * FROM products");
+    // Modify the SQL query to include the search condition
+    $stmt = $pdo->prepare("SELECT * FROM products WHERE productName LIKE :searchQuery");
+    $stmt->bindValue(':searchQuery', "%$searchQuery%", PDO::PARAM_STR);
+    $stmt->execute();
+
+    // Fetch products as an associative array
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Output XML header
-    header('Content-Type: application/xml');
+    // Output the XML response
+    header('Content-Type: text/xml');
     echo '<?xml version="1.0" encoding="UTF-8"?>';
     echo '<products>';
-
-    // Output each product as XML
     foreach ($products as $product) {
         echo '<product>';
         echo '<productName>' . htmlspecialchars($product['productName']) . '</productName>';
@@ -20,7 +27,6 @@ try {
         echo '<sales>' . htmlspecialchars($product['sales']) . '</sales>';
         echo '</product>';
     }
-
     echo '</products>';
 } catch (PDOException $e) {
     die("Error retrieving products: " . $e->getMessage());
--- pages/products.php
+++ pages/products.php
@@ -1,51 +1,60 @@
 <div class="content-container">
     <h1 class="font-weight-bold">Products</h1>
     <div class="search-container">
-        <form action="index.php?page=products" method="GET">
-            <input type="text" name="search" placeholder="Search products...">
-            <button type="submit">Search</button>
+        <form id="searchForm">
+            <input type="text" id="searchInput" name="search" placeholder="Search products...">
+            <button type="button" onclick="searchProducts()">Search</button>
         </form>
     </div>
     <div id="products-container" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3"></div>
 </div>
 
 <script>
-    // Make an AJAX request to fetch products in XML format
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Parse XML response
-            var xmlDoc = xhr.responseXML;
-            var products = xmlDoc.getElementsByTagName("product");
+    function searchProducts() {
+        // Get the search query from the input field
+        var searchQuery = document.getElementById("searchInput").value;
 
-            // Display products on the screen
-            var productsContainer = document.getElementById("products-container");
-            for (var i = 0; i < products.length; i++) {
-                var product = products[i];
-                var productName = product.getElementsByTagName("productName")[0].textContent;
-                var price = product.getElementsByTagName("price")[0].textContent;
-                var image = product.getElementsByTagName("image")[0].textContent;
-                var sales = product.getElementsByTagName("sales")[0].textContent;
+        // Make an AJAX request to fetch products based on the search query
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Parse XML response
+                var xmlDoc = xhr.responseXML;
+                var products = xmlDoc.getElementsByTagName("product");
 
-                var cardHtml = '<div class="col">';
-                cardHtml += '<div class="card col-lg-4" style="width: 18rem;">';
-                cardHtml += '<img src="' + image + '" class="card-img-top" alt="' + productName + '">';
-                cardHtml += '<div class="card-body">';
-                cardHtml += '<h5 class="card-title card-title-constraint">' + productName + '</h5>';
-                cardHtml += '<div class="card-content">';
-                cardHtml += '<span class="card-text">$' + price + '</span>';
-                cardHtml += '<span class="card-text">Sales: ' + sales + '</span>';
-                cardHtml += '</div>';
-                cardHtml += '<a href="#" class="btn btn-primary">Buy</a>';
-                cardHtml += '</div>';
-                cardHtml += '</div>';
-                cardHtml += '</div>';
+                // Display products on the screen
+                var productsContainer = document.getElementById("products-container");
+                productsContainer.innerHTML = ""; // Clear previous results
 
-                productsContainer.innerHTML += cardHtml;
+                for (var i = 0; i < products.length; i++) {
+                    var product = products[i];
+                    var productName = product.getElementsByTagName("productName")[0].textContent;
+                    var price = product.getElementsByTagName("price")[0].textContent;
+                    var image = product.getElementsByTagName("image")[0].textContent;
+                    var sales = product.getElementsByTagName("sales")[0].textContent;
+
+                    var cardHtml = '<div class="col">';
+                    cardHtml += '<div class="card col-lg-4" style="width: 18rem;">';
+                    cardHtml += '<img src="' + image + '" class="card-img-top" alt="' + productName + '">';
+                    cardHtml += '<div class="card-body">';
+                    cardHtml += '<h5 class="card-title card-title-constraint">' + productName + '</h5>';
+                    cardHtml += '<div class="card-content">';
+                    cardHtml += '<span class="card-text">$' + price + '</span>';
+                    cardHtml += '<span class="card-text">Sales: ' + sales + '</span>';
+                    cardHtml += '</div>';
+                    cardHtml += '<a href="#" class="btn btn-primary">Buy</a>';
+                    cardHtml += '</div>';
+                    cardHtml += '</div>';
+                    cardHtml += '</div>';
+
+                    productsContainer.innerHTML += cardHtml;
+                }
             }
-        }
-    };
+        };
 
-    xhr.open("GET", "./get-products-in-xml.php", true);
-    xhr.send();
+        xhr.open("GET", "./get-products-in-xml.php?search=" + encodeURIComponent(searchQuery), true);
+        xhr.send();
+    }
+
+    searchProducts();
 </script>
